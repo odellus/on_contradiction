8afed70926945d731b8c7c2b3bc17d892895f940
Thomas Wood <odell.wood@gmail.com>
2022-04-12 12:41:46 -0700

    Create fetch_model.sh

---
+++ fetch_model.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git lfs clone https://huggingface.co/google/pegasus-large models/pegasus-large
+git lfs clone https://huggingface.co/allenai/longformer-large-4096 models/longformer-large-4096
+git lfs clone https://huggingface.co/google/bigbird-roberta-large models/bigbird-roberta-large
